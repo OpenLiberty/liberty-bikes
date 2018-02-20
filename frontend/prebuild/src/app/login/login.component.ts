ef33c25cd68c8e75072ded419dea27d4675ff57d
Liam Westby <liamwestby@icloud.com>
2018-02-20 09:11:47 -0600

    Convert to styles to SASS

--- frontend/prebuild/src/app/login/login.component.ts
+++ frontend/prebuild/src/app/login/login.component.ts
@@ -6,7 +6,7 @@ import * as $ from 'jquery';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
   constructor(private router: Router, private ngZone: NgZone) {
