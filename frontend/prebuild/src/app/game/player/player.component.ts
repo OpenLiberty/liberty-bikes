a14608fb747cd88a6c30e220fa8f4d63b935158c
Liam Westby <liamwestby@icloud.com>
2018-04-26 09:16:12 -0500

    Refactor player to only hold properties, player component handles styles

--- frontend/prebuild/src/app/game/player/player.component.ts
+++ frontend/prebuild/src/app/game/player/player.component.ts
@@ -14,4 +14,38 @@ export class PlayerComponent implements OnInit {
   ngOnInit() {
   }
 
+  public get style(): any {
+    const style = {
+      'border-left': `5px solid ${this.player.color}`,
+      'background-color': `${this.backgroundColor(this.player.color)}`
+    };
+
+    return style;
+  }
+
+  public get color(): string {
+    if (this.player.status === 'Dead' || this.player.status === 'Disconnected') {
+      return '#BBBBBB';
+    }
+
+    return this.player.color;
+  }
+
+  get statusImage() {
+    var status = this.player.status.toLowerCase();
+    if (status === 'none') {
+      return '';
+    }
+
+	  if (status === 'connected') {
+      status = 'alive';
+    }
+
+	  return `/assets/images/status_${status}.png`;
+  }
+
+  backgroundColor(color: string): string {
+    return `${color}30`;
+  }
+
 }
