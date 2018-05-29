fbfb748f8b8ff9ed3f14094ea28ac7545e6610be
Liam Westby <liamwestby@icloud.com>
2018-05-29 14:30:49 -0500

    Horizontal animation

--- frontend/prebuild/src/app/game/leaderboard/leaderboard.component.ts
+++ frontend/prebuild/src/app/game/leaderboard/leaderboard.component.ts
@@ -12,12 +12,12 @@ import { trigger, transition, style, animate } from '@angular/animations';
   animations: [
     trigger('swap', [
       transition(':enter', [
-        style({transform: 'translateY(100%)', opacity: 0}),
-        animate('1000ms', style({transform: 'translateY(0)', opacity: 1}))
+        style({transform: 'translateX(100%)', opacity: 0}),
+        animate('1000ms', style({transform: 'translateX(0)', opacity: 1}))
       ]),
       transition(':leave', [
-        style({transform: 'translateY(0)', opacity: 1}),
-        animate('1000ms', style({transform: 'translateY(-100%)', opacity: 0}))
+        style({transform: 'translateX(0)', opacity: 1}),
+        animate('1000ms', style({transform: 'translateX(-100%)', opacity: 0}))
       ])
     ])
   ]
