8f094ac5c4bf79fd4f35805b8826ab263ba0816f
Andrew Guibert <andy.guibert@gmail.com>
2018-10-03 11:11:25 -0500

    Adjust script paths based on /Users/aguibert value

--- game-service/src/main/liberty/config/.profile.d/cf_setup.sh
+++ game-service/src/main/liberty/config/.profile.d/cf_setup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This script installs the required features when the server is deployed as a Cloud Foundry application
-export PATH=$PATH:$HOME/app/.java/jre/bin
+export PATH=$PATH:$HOME/.java/jre/bin
 echo "Path is $PATH"
 java -version
 
 echo "Installing required features for server..."
-$HOME/app/.liberty/bin/installUtility install $HOME/app/wlp/usr/servers/defaultServer/server.xml --acceptLicense
+$HOME/.liberty/bin/installUtility install $HOME/wlp/usr/servers/defaultServer/server.xml --acceptLicense
