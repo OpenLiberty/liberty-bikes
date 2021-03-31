4b849fa0f35f43f12cafb6da1cfd52f66115d9b3
Liam Westby <liamwestby@icloud.com>
2021-03-31 13:17:16 -0500

    update @angular/core

--- frontend/prebuild/src/app/game/game.component.spec.ts
+++ frontend/prebuild/src/app/game/game.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { GameComponent } from './game.component';
 
@@ -6,7 +6,7 @@ describe('GameComponent', () => {
   let component: GameComponent;
   let fixture: ComponentFixture<GameComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ GameComponent ]
     })
--- frontend/prebuild/src/app/game/playerlist/playerlist.component.spec.ts
+++ frontend/prebuild/src/app/game/playerlist/playerlist.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PlayerListComponent } from './playerlist.component';
 
@@ -6,7 +6,7 @@ describe('PlayerlistComponent', () => {
   let component: PlayerListComponent;
   let fixture: ComponentFixture<PlayerListComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PlayerListComponent ]
     })
