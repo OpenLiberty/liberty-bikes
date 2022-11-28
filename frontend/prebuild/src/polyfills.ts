332a8fd48059110347503bb7b04a016aec9e1cc7
Liam Westby <liamwestby@icloud.com>
2022-11-28 14:35:30 -0600

    Update to Angular 13

--- frontend/prebuild/src/polyfills.ts
+++ frontend/prebuild/src/polyfills.ts
@@ -1,15 +1,7 @@
 /***************************************************************************************************
  * Load `$localize` onto the global scope - used if i18n tags appear in Angular templates.
  */
-import '@angular/localize/init';
-
-
-
-/**
- * Required to support Web Animations `@angular/platform-browser/animations`.
- * Needed for: All but Chrome, Firefox and Opera. http://caniuse.com/#feat=web-animation
- **/
-import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+import '@angular/localize/init';  // Run `npm install --save web-animations-js`.
 
 
 
